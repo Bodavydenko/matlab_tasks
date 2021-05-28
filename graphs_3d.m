1be89b1ad5bb9538280ed8f8cf09c1458dc84b0f
Bodavydenko <84570918+Bodavydenko@users.noreply.github.com>
2021-05-29 01:24:11 +0300

    Исправлено, доделано

--- graphs_3d.m
+++ graphs_3d.m
@@ -1,3 +1,41 @@
+%% %Task1/1
+t=0:0.05:9*pi;
+x=2*sin(t);y=3*cos(t);
+plot3(x,y,t)
+%% %Task1/2
+t=0:0.05:9*pi;
+x=2*sin(t);y=3*cos(t);
+plot3(x,y,t)
+t0=7*pi/2;
+hold on;
+fx=@(t) 2*cos(t);
+fy=@(t) -3*sin(t);
+line([2*sin(t0),fx(t0)+2*sin(t0)],[3*cos(t0),fy(t0)*2+3*cos(t0)],[t0,t0]);
+plot3(2*sin(t0),3*cos(t0),t0,'x')
+hold off;
+%% %Task1/3
+n=1000
+t1=pi.*(-n:5:n)/n;
+t2=(pi/2)*(-n:5:n)'/n;
+X=cos(t2)*cos(t1);
+Y=cos(t2)*sin(t1);
+E=ones(size(t1));
+Z=sin(t2)*E;
+plot3(X,Y,Z,'b')
+%% %Example
+[x,y]=meshgrid(linspace(-pi,pi,100),linspace(-pi,pi,100));
+z=20-x.^2-y.^2;
+surf(x,y,z);
+hold on;
+phi=linspace(0,6*pi,300);
+x=0.1.*phi.*cos(phi);
+y=0.1.*phi.*sin(phi);
+line([0,0],[-1,-3],[19,20]);
+z=20-x.^2-y.^2;
+plot3(x,y,z);
+z=0.*z;
+plot3(x,y,z);
+hold off;
 %% %Task1
 a=2;
 b=1;
