959d317600a6df6a82c7cf3c0d7ae13268b27a00
Bodavydenko <84570918+Bodavydenko@users.noreply.github.com>
2021-06-02 03:02:50 +0300

    Исправлено

--- GraphTask/GraphTask.m
+++ GraphTask/GraphTask.m
@@ -4,11 +4,11 @@ graph=digraph(edges.Source,edges.Target,edges.Weight);
 graph_plot=plot(graph,"Layout","auto");
 graph.Nodes.NodeSizes=2.*sqrt(indegree(graph)-min(indegree(graph))+0.2);
 graph_plot.MarkerSize=graph.Nodes.NodeSizes;
-graph.Nodes.NodeColors=2.*sqrt(indegree(graph)-min(indegree(graph))+0.2);
-graph_plot.NodeCData=graph.Nodes.NodeColors;
+graph_plot.NodeCData=graph.Nodes.NodeSizes;
+graph.Nodes.NodeIn=indegree(graph);
 colorbar
 shortPath=shortestpath(graph,'NED','TYRION');
 highlight(graph_plot,shortPath,"EdgeColor","r")
 disp("Самый короткий путь между NED и TYRION:")
 disp(shortPath)
-disp(head(sortrows(table(graph.Nodes.Name,graph.Nodes.NodeSizes),"Var2","descend"),5))
+disp(head(sortrows(table(graph.Nodes.Name,graph.Nodes.NodeIn),"Var2","descend"),5))
